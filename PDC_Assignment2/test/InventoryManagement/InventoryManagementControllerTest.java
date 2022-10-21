0190a10d8ec8b0703260ea66c16b6de451d27160
catcodes02 <84115671+catcodes02@users.noreply.github.com>
2022-10-21 13:55:32 +1300

    Controller & Categories Tests

---
+++ PDC_Assignment2/test/InventoryManagement/InventoryManagementControllerTest.java
@@ -0,0 +1,46 @@
+package InventoryManagement;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class InventoryManagementControllerTest {
+
+    InventoryManagementController instance;
+
+    public InventoryManagementControllerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        instance = new InventoryManagementController();
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of changeLocation method, of class InventoryManagementController.
+     */
+    @Test
+    public void testChangeLocation() {
+        System.out.println("changeLocation");
+
+        InventoryManagementController.Location location = InventoryManagementController.Location.HOME;
+        instance.changeLocation(location);
+
+        assertEquals(location, instance.currentLocation);
+    }
+}
